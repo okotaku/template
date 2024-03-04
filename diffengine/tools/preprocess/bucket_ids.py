8227e7ec904dbf899df25efa46a65aa92fd7fb87
okotaku <to78314910@gmail.com>
2024-03-04 07:03:49 +0000

    Update bucket ids script

--- diffengine/tools/preprocess/bucket_ids.py
+++ diffengine/tools/preprocess/bucket_ids.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path as osp
+from pathlib import Path
 
 import joblib
 import mmengine
@@ -8,6 +9,8 @@ import pandas as pd
 from mmengine.config import Config
 from PIL import Image
 from tqdm import tqdm
+
+from diffengine.configs import cfgs_name_path
 
 
 def parse_args():
@@ -23,14 +26,27 @@ def parse_args():
 def main() -> None:
     args = parse_args()
 
+    if not Path(args.config).exists():
+        try:
+            args.config = cfgs_name_path[args.config]
+        except KeyError as exc:
+            msg = f"Cannot find {args.config}"
+            raise FileNotFoundError(msg) from exc
+
     cfg = Config.fromfile(args.config)
     data_dir = cfg.train_dataloader.dataset.get("dataset")
     img_dir = cfg.train_dataloader.dataset.get("img_dir", "")
     csv = cfg.train_dataloader.dataset.get("csv", "metadata.csv")
+    image_column = cfg.train_dataloader.dataset.get("image_column", "image")
     csv_path = osp.join(data_dir, csv)
     img_df = pd.read_csv(csv_path)
 
-    sizes = cfg.train_dataloader.dataset.pipeline[0].get("sizes")
+    i = 0
+    while True:
+        if hasattr(cfg.train_dataloader.dataset.pipeline[i], "sizes"):
+            sizes = cfg.train_dataloader.dataset.pipeline[i].get("sizes")
+            break
+        i += 1
     aspect_ratios = np.array([s[0] / s[1] for s in sizes])
 
     def get_bucket_id(file_name):
@@ -41,7 +57,7 @@ def main() -> None:
 
     bucket_ids = joblib.Parallel(n_jobs=args.n_jobs, verbose=10)(
         joblib.delayed(get_bucket_id)(file_name)
-        for file_name in tqdm(img_df.file_name.values))
+        for file_name in tqdm(img_df[image_column].values))
 
     print(pd.DataFrame(bucket_ids).value_counts())
 
